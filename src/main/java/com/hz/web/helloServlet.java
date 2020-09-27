e066168689b7199cf6bcbce4ade9ef800c347783
taotao <goodMorning_pro@hz.com>
2020-09-27 11:32:00 +0800

    我吃饭了

---
+++ src/main/java/com/hz/web/helloServlet.java
@@ -0,0 +1,27 @@
+package com.hz.web;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class helloServlet
+ */
+public class helloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+        
+    
+  
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	     System.out.println("我的第一个...............maven");
+	
+	}
+	  
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
